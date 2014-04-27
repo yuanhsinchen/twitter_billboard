84d19ec591ef8735061b67fafa3dc732b41af108
Yuan-Hsin Chen <yuanlmm@gmail.com>
2014-04-27 01:08:57 -0400

    merged scraping billboard and youtube

--- scrape.py
+++ scrape.py
@@ -1,31 +1,50 @@
 import lxml.html
+import json
 
+"""
+1. Scrape Billboard
+ - Song's name
+ - Singer
+ - Weeks on chart
+2. Search song's name in YouTube
+ - YouTube fingerprint of a song
+3. combine above
+ - [{'song':'name', 'singe':'name', 'weeks':int, 'fingerprint':'fp'}, ...]
+3. encoder as json object
+"""
 #top1-10
 s = "http://www.billboard.com/charts/hot-100"
 doc = lxml.html.parse(s)
-top100 = doc.xpath("//header/h1/text()")
-top100 = [s.strip() for s in top100]
-top100s = doc.xpath("//header/p/a/text()")
-top100s = [s.strip() for s in top100s]
+hot100 = []
+for node in doc.xpath("//article/header"):
+    hot = {}
+    hot['song'] = ''.join(node.xpath("h1/text()")).strip()
+    hot['singer'] = ''.join(node.xpath("p/a/text()")).strip()
+    hot['weeks'] = int(''.join(node.xpath("ul/li[3]/text()")).strip())
+    hot100.append(hot)
 
 #top11-100
 for i in range(1, 10):
     s = "http://www.billboard.com/charts/hot-100?page=" + str(i)
     doc = lxml.html.parse(s)
-    top = doc.xpath("//header/h1/text()")
-    top = [s.strip() for s in top]
-    for s in top:
-        top100.append(s)
-
-    tops = doc.xpath("//header/p/a/text()")
-    tops = [s.strip() for s in tops]
-    for s in tops:
-        top100s.append(s)
-
-top100 += top100s
-
-f = open('keywords.txt', 'w+')
-for s in top100:
-    f.write(s + '\n')
+    for node in doc.xpath("//article/header"):
+        hot = {}
+        hot['song'] = ''.join(node.xpath("h1/text()")).strip()
+        hot['singer'] = ''.join(node.xpath("p/a/text()")).strip()
+        hot['weeks'] = int(''.join(node.xpath("ul/li[3]/text()")).strip())
+        hot100.append(hot)
+#YouTube fingerprints
+f = open('track/kw20140503.txt', 'wb')
+for hot in hot100:
+    y = "http://www.youtube.com/results?search_query=" + hot['song'].replace(' ', '+')
+    doc = lxml.html.parse(y)
+    hot['fingerprint'] = ''.join(doc.xpath("//div/a/@href")[2]).replace('/watch?v=', '')
+    print hot['fingerprint']
+    f.write(hot['fingerprint'] + '\n')
 f.close()
-print top100
+
+#encoder as json
+j = json.dumps(hot100)
+fjson = open('track/20140503.json', 'wb')
+print >> fjson, j
+fjson.close()
